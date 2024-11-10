3d635fcac3c085d174f1c56f5c298ef88d4dc2b4
Daria Ingatiuk <Ignatyuk.dariaa@gmail.com>
2024-11-10 10:47:59 +0200

    added log

--- config/db.js
+++ config/db.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT, PROD} = process.env;
 
+console.log(PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT, PROD);
+
+
 module.exports = {
     db: knex({
         client: 'pg',
